381768d889db0820ac69c49dc24864cecd5a5521
Divya R <rd9636542@gmail.com>
2025-08-09 12:32:58 +0530

    first commit

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+session_destroy();
+header("Location:signin.php")
+?>
+OUTPUT:
+Signin page
+Signup page
+Database
+Forgot password page
+Chat Page
+Home Page
+Settings Page
+Change Password Page
+Upload Profile Page
+Chatting Page
+Database
+RESULT:
+Thus the program to create web-based application using PHP,CSS,Database with MySql was written and executed successfully.
